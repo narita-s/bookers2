ae9d99be7a006f117a9b717c6a02a8be60901735
EC2 Default User <ec2-user@ip-172-31-93-70.ec2.internal>
2022-05-21 13:31:12 +0000

    [Update] 投稿削除機能追加　

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,25 +6,39 @@ class BooksController < ApplicationController
   # 投稿データの保存
   def create
     @book = Book.new(book_params)
-    @Book.user_id = current_user.id
-    @book.save
-    redirect_to book_path
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully created."
+    else
+      @books = Book.all
+      render :index
+    end
   end
-  
+    
   def index
+    @books = Book.all
   end
 
   def show
+    @book= Book.find(params[:id])
   end
 
   def edit
   end
   
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to '/books'
+  end
+
+  
   # 投稿データのストロングパラメータ
   private
 
   def book_params
-    params.require(:book).permit(:tytle, :body)
+    params.require(:book).permit(:title, :body)
   end
   
 end
